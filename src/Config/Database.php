9b09ac89d513df0ee8b0837df255fddad96dbdae
pinashi <yabloko.073@gmail.com>
2026-05-31 23:52:05 +0300

    Add try/catch and PHPDoc to Database class

--- src/Config/Database.php
+++ src/Config/Database.php
@@ -18,6 +18,12 @@ Class Database {
     private static ?PDO $connection = null;
 
     /**
+     * Private constructor to prevent direct instantiation.
+     * Use getConnection() to get the database connection.
+     */
+    private function __construct() {}
+    
+    /**
      * Get the database connection instance.
      * Creates a new connection if one does not exist.
      *
@@ -26,14 +32,19 @@ Class Database {
      */
     public static function getConnection(): PDO {
         if (self::$connection === null) {
-            $env = parse_ini_file(__DIR__ . '/../.env');
+            try {
+                $env = parse_ini_file(__DIR__ . '/../.env');
 
-            self::$connection = new PDO(
-                "mysql:host={$env['DB_HOST']};dbname={$env['DB_NAME']};charset=utf8",
-                $env['DB_USER'],
-                $env['DB_PASSWORD'],
-                [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
-            );
+                self::$connection = new PDO(
+                    "mysql:host={$env['DB_HOST']};dbname={$env['DB_NAME']};charset=utf8",
+                    $env['DB_USER'],
+                    $env['DB_PASSWORD'],
+                    [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+                );
+            } catch (PDOException $e) {
+                error_log($e->getMessage());
+                die('Database connection failed');
+            }
         }
     
         return self::$connection;
